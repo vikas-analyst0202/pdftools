8ab707d4cd70f020fb5a79a523c39fb9c09248fb
vikas-analyst0202 <vikashdon0202+1@gmail.com>
2026-02-02 00:14:54 +0530

    Add PWA support and offline libraries

---
+++ service-worker.js
@@ -0,0 +1,65 @@
+const CACHE_NAME = 'pdf-master-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './pdf-tools.js',
+    './libs/lucide.js',
+    './libs/pdf-lib.min.js',
+    './libs/jszip.min.js'
+];
+
+// Install Event: Cache all assets
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Opened cache');
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+// Activate Event: Cleanup old caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Clearing old cache', cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Event: Serve from cache, fallback to network
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            // Clone the request to make a fetch
+            return fetch(event.request).then((response) => {
+                // Check if we received a valid response
+                if (!response || response.status !== 200 || response.type !== 'basic') {
+                    return response;
+                }
+
+                // Clone the response to put in cache
+                const responseToCache = response.clone();
+
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseToCache);
+                });
+
+                return response;
+            });
+        })
+    );
+});
